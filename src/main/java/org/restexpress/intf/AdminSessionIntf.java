ed31b99fef603f01fb633ef92de659e114566af7
hanst <hanst@54cf8e18-d8ae-4a62-b659-92dc5b0afa03>
2017-06-24 05:56:16 +0000

    调整了回话实现的获取

--- src/main/java/org/restexpress/intf/AdminSessionIntf.java
+++
@@ -1,8 +0,0 @@
-package org.restexpress.intf;
-
-import org.restexpress.domain.ex.AdminSessionInfo;
- 
-
-public interface AdminSessionIntf<AdminUserSessionInfo> extends SessionIntf<AdminSessionInfo> {
-   
-}
