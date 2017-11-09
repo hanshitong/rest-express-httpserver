6e3aac2071f6317dc34e68c22d5462abcbb7b335
hanshitong <33052617+hanshitong@users.noreply.github.com>
2017-11-09 14:56:55 +0800

    Update HttpRequestParameterParseIntf.java

--- src/main/java/org/restexpress/intf/HttpRequestParameterParseIntf.java
+++ src/main/java/org/restexpress/intf/HttpRequestParameterParseIntf.java
@@ -8,5 +8,5 @@ import org.restexpress.exception.ServiceException;
 
 public interface HttpRequestParameterParseIntf {
 	//从客户端http请求request里解析目标方法action需要的参数,并按顺序以数组方式返回
-	Object[] parse(Method action,Request request,Response response) throws ServiceException;
+	Object[] parse(Method action,Request request,Response response) throws Exception;
 }
